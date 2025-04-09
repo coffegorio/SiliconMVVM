9f38234bb433b8cea0e09899c2c102d09d989d97
coffegorio <coffegorio@vk.com>
2025-04-09 22:44:55 +0300

    Initial Commit

---
+++ SiliconMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiliconMVVM
+//
+//  Created by Егорио on 09.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
